9e910ea8775a19e3bfc80a102ed8755cca6f0739
anamklone <amk226@pitt.edu>
2018-02-03 12:52:31 -0500

    try to fix issues with pitch and yaw incorrect/correct

--- webapp/script.js
+++ webapp/script.js
@@ -1,5 +1,5 @@
 Myo.connect('com.example.appId');
-
+	
 Myo.on('connected', function() {
 	console.log('connected');
 });
@@ -21,45 +21,57 @@ Myo.on('rest', function() {
 	console.log('rest');
 });
 
-const HI = 110;
-const LO = -110;
-const FLUX = 1;
+var LO = -60;// = -110;
+var HI = 260;// = 110;
+var FLUX = 1;// = 1;
 
 var armUp = true;
-var prevPitch = LO;
+var prevPitch = 0;// = LO;
+var prevYaw = -160;// = -100;
 
 Myo.on('orientation', function(data) {
-	//console.log(data);
-	
 	var roll = Math.atan2(2.0 * (data.w * data.x + data.y * data.z), 1.0 - 2.0 * (data.x * data.x + data.y * data.y)) * 100;
 	
 	var pitch = Math.asin(Math.max(-1.0, Math.min(1.0, 2.0 * (data.w * data.y - data.z * data.x)))) * 100;
 	
 	var yaw = Math.atan2(2.0 * (data.w * data.z + data.x * data.y), 1.0 - 2.0 * (data.y * data.y + data.z * data.z)) * 100;
 
-	//console.log("roll = " + roll);
+	console.log("roll = " + roll);
 	console.log("pitch = " + pitch);
-	//console.log("yaw = " + yaw);
+	console.log("yaw = " + yaw);
+	
+	/*if (Math.abs(yaw - prevYaw) >= 50) {
+		console.log("incorrect yaw");
+	} else {
+		if (armUp) {
+			if (prevPitch <= pitch + FLUX){
+				console.log("correct pitch");
+			} else if (pitch <= LO) {
+				console.log("correct pitch");
+			} else {
+				console.log("incorrect pitch");
+			}
+		} else {
+			if (prevPitch >= pitch + FLUX) {
+				console.log("correct pitch");
+			} else if (pitch >= HI) {
+				console.log("correct pitch");
+			} else {
+				console.log("incorrect pitch");
+			}
+		}	
+		console.log("correct yaw");
+	}
 	
 	if (prevPitch >= HI) {
 		armUp = false;
 	} else if (prevPitch <= LO) {
 		armUp = true;
 	}
-		
-	if (armUp) {
-		if (prevPitch <= pitch || prevPitch <= pitch + FLUX) {
-			console.log("correct");
-		} else {
-			console.log("incorrect");
-		}
-	} else {
-		if (prevPitch >= pitch || prevPitch >= pitch - FLUX) {
-			console.log("correct");
-		} else {
-			console.log("incorrect");
-		}
-	}
 	
-	prevPitch = pitch;
+	prevPitch = pitch;*/
 });
+
+function calibrate() {
+	Myo.myos[0].zeroOrientation();
+}
