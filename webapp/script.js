e25f7cc6f10e5a692e0b5954e03e77f9c01bd368
anamklone <amk226@pitt.edu>
2018-02-03 10:26:44 -0500

    check for correct movement when moving arm up and down (based on pitch)

--- webapp/script.js
+++ webapp/script.js
@@ -21,16 +21,45 @@ Myo.on('rest', function() {
 	console.log('rest');
 });
 
-Myo.on('orientation', function(data) {
-	console.log(data);
-	
-	var roll = Math.atan2(2.0 * (data.w * data.x + data.y * data.z), 1.0 - 2.0 * (data.x * data.x + data.y * data.y));
-	
-	var pitch = Math.asin(Math.max(-1.0, Math.min(1.0, 2.0 * (data.w * data.y - data.z * data.x))));
-	
-	var yaw = Math.atan2(2.0 * (data.w * data.z + data.x * data.y), 1.0 - 2.0 * (data.y * data.y + data.z * data.z));
+const HI = 110;
+const LO = -110;
+const FLUX = 1;
 
-	console.log("roll = " + roll);
+var armUp = true;
+var prevPitch = LO;
+
+Myo.on('orientation', function(data) {
+	//console.log(data);
+	
+	var roll = Math.atan2(2.0 * (data.w * data.x + data.y * data.z), 1.0 - 2.0 * (data.x * data.x + data.y * data.y)) * 100;
+	
+	var pitch = Math.asin(Math.max(-1.0, Math.min(1.0, 2.0 * (data.w * data.y - data.z * data.x)))) * 100;
+	
+	var yaw = Math.atan2(2.0 * (data.w * data.z + data.x * data.y), 1.0 - 2.0 * (data.y * data.y + data.z * data.z)) * 100;
+
+	//console.log("roll = " + roll);
 	console.log("pitch = " + pitch);
-	console.log("yaw = " + yaw);
+	//console.log("yaw = " + yaw);
+	
+	if (prevPitch >= HI) {
+		armUp = false;
+	} else if (prevPitch <= LO) {
+		armUp = true;
+	}
+		
+	if (armUp) {
+		if (prevPitch <= pitch || prevPitch <= pitch + FLUX) {
+			console.log("correct");
+		} else {
+			console.log("incorrect");
+		}
+	} else {
+		if (prevPitch >= pitch || prevPitch >= pitch - FLUX) {
+			console.log("correct");
+		} else {
+			console.log("incorrect");
+		}
+	}
+	
+	prevPitch = pitch;
 });
